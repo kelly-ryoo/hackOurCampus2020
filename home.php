b3b021f8fb8d374fb031202ceecbfd850bf651ec
kelly-ryoo <kyyr.0317@gmail.com>
2020-08-28 11:56:17 -0400

    added home page

---
+++ home.php
@@ -0,0 +1,21 @@
+    <!-- WELCOME -->
+    <div class="row">
+        <h3 class="home_header" id="welcome">welcome</h3>
+    </div>
+
+    <!-- WEBSITE TITLE -->
+    <div class="row">
+        <h1 class="home_header" id="web_title">webname</h1>
+    </div>
+    
+    <!-- LOG IN BUTTON -->
+    <div class="row home-bt-1">
+        <div class="col-sm-4"></div>
+        <button class="col-sm-4 home-button"><a href="log_in.html">log in</a></button>
+    </div>
+
+    <!-- SIGN UP BUTTON -->
+    <div class="row home-bt-2">
+        <div class="col-sm-4"></div>
+        <button class="col-sm-4 home-button"><a href="sign_up.html">sign up</a></button>
+    </div>
